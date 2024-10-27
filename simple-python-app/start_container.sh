920f8b1c78fe812f7f5b2105a2120c9f7563b1da
Shubham Kshetre <shubhamkshetre@hotmail.com>
2024-10-28 00:08:45 +0530

    Update start_container.sh

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 557690596766.dkr.ecr.us-east-1.amazonaws.com
+
 # Pull the Docker image from Docker Hub
-echo
+echo "Pull the Docker image from Docker Hub"
+docker pull 557690596766.dkr.ecr.us-east-1.amazonaws.com/python-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 557690596766.dkr.ecr.us-east-1.amazonaws.com/python-app
